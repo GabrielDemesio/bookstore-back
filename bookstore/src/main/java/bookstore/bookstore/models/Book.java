214595e8d5214863eab6eaad811f5477dc0ace57
Antox23 <cfmoneygamer@gmail.com>
2023-09-14 21:06:01 -0300

    "Project library Back-End SpringBoot Java"

---
+++ bookstore/src/main/java/bookstore/bookstore/models/Book.java
@@ -0,0 +1,42 @@
+package bookstore.bookstore.models;
+
+import java.sql.Date;
+
+import org.springframework.validation.annotation.Validated;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+
+
+@Validated
+@Entity
+@Data
+@Table(name = "book")
+@Getter
+@Setter 
+public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    
+    @NotBlank(message="Titulo deve ser definido")
+    @Column(nullable = false, name = "title")
+    private String title;
+    
+    @NotBlank(message ="Nome do autor deve ser definido")
+    @Column(nullable = false, name = "author")
+    private String author;
+    
+    @NotBlank(message = "Texto tem que ser definido")
+    @Column(nullable = false, name = "text")
+    private String text;
+    
+}
